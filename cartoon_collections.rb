755f45633c5b849de1629e38b620284cc060b888
Joshua Williams <jmw0426@hotmail.com>
2020-03-27 20:51:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,9 @@ def long_planeteer_calls(array)
   i = 5
   if array.count |5| 5 >= i
     return true 
+  else array.count |5| 5 >= i
   end 
-  false
+
 end
 
 def find_the_cheese# code an argument here
