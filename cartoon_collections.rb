d7e85c5ccc2acb5a2eb1957c76dbd95890e30ad3
Joshua Williams <jmw0426@hotmail.com>
2020-03-27 19:47:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
    names.map(&:capitalize)
      names << names
      names.map { |word| “#{word}” }
-
+end
 end
 
 def long_planeteer_calls# code an argument here
