f3d7bc4ba44336ea2cc588b47d34693cad1b1a38
Joshua Williams <jmw0426@hotmail.com>
2020-03-27 20:28:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,10 +19,8 @@ def long_planeteer_calls(array)
   i = 4
   if array.length >= i
     return true 
-  elsif array.length == i - 1
-    return false
   else array.length == i
-    return false 
+    return true 
   end  
 end
 
