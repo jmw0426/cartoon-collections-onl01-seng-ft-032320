bb083f68908c9cf1ecdf150b5c7e3cf563a1f2a6
Joshua Williams <jmw0426@hotmail.com>
2020-03-27 20:18:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,12 @@ def summon_captain_planet(array)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.length > 4 
+    return true 
+  else 
+    return false 
+  end  
 end
 
 def find_the_cheese# code an argument here
